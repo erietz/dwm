965ab43d738caa97a53b5a507d7d3c03e4ec7f5b
Ethan Rietz <ewrietz@gmail.com>
2021-09-13 16:50:01 -0500

    change default terminal to st

--- config.def.h
+++ config.def.h
@@ -43,9 +43,9 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    /* class      instance    title      tags mask     isfloating   monitor */
+    { "Gimp",     NULL,       NULL,      0,            1,           -1 },
+    { "Firefox",  NULL,       NULL,      1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accent, "-sf", col_gray1, NULL };
-static const char *termcmd[]  = { "alacritty" };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
